9ab0222992181e9ac51dcef2114706f711be45de
Leon <jiangxilee@gmail.com>
2016-09-27 11:27:10 +0800

    add -y for apt-get install command.

--- init-ubuntu-16.04.sh
+++ init-ubuntu-16.04.sh
@@ -3,8 +3,8 @@
 pwd=`pwd`
 cd /tmp
 
-sudo apt-get install openssh-server
-sudo apt-get install git
+sudo apt-get install -y openssh-server
+sudo apt-get install -y git
 
 wget -c http://drycms.hk.ufileos.com/docker-1.11.2.tgz -O docker.tgz
 mkdir docker && tar -zxvf docker.tgz -C ./docker --strip-components 1
@@ -16,7 +16,7 @@ wget -c http://drycms.hk.ufileos.com/docker-compose-1.8.0 -O docker-compose
 sudo mv docker-compose /usr/bin
 sudo chmod a+x /usr/bin/docker-compose
 
-sudo apt-get install aufs-tools
+sudo apt-get install -y aufs-tools
 
 sudo rm -rf /usr/bin/run-server
 sudo ln -s ${pwd}/run-server.sh /usr/bin/run-server
