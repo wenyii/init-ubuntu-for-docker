a92210cae816e3162567e1a2428f7d864741e6cf
Leon <jiangxilee@gmail.com>
2016-10-04 00:03:04 +0800

    add .vimrc file.

--- software.sh
+++ software.sh
@@ -7,9 +7,26 @@ sudo apt-get install mysql-client-core-5.7
 sudo apt-get install -y openssh-server
 
 # Init ssh
-ssh-keygen -t rsa -P ''
-sudo touch ~/.ssh/authorized_keys
-sudo chmod 600 ?_
+if [ ! -d ~/.ssh ]
+then
+    ssh-keygen -t rsa -P ''
+fi
+
+if [ ! -f ~/.ssh/authorized_keys ]
+then
+    sudo touch ~/.ssh/authorized_keys
+    sudo chmod 600 ?_
+fi
 
 # Git
 sudo apt-get install -y git
+
+# Vim config
+cat > ~/.vimrc << EOF
+syntax on
+set number
+set tabstop=4
+set expandtab
+set softtabstop=4
+set shiftwidth=4
+EOF
