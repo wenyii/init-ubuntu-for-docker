31a8e84a99b57bef2529b7c5364a470424ecdff2
Leon <jiangxilee@gmail.com>
2016-10-08 08:53:43 +0800

    add software vim

--- software.sh
+++ software.sh
@@ -23,12 +23,16 @@ if [ ! -f ~/.ssh/authorized_keys ]
 then
     echo -e "\n\033[1;34m ---------- create ssh auth file ----------\033[0;0m\n"
     sudo touch ~/.ssh/authorized_keys
-    sudo chmod 600 ?_
+    sudo chmod 600 ~/.ssh/authorized_keys
 fi
 
 # Git
 echo -e "\n\033[1;34m  ---------- install git ----------\033[0;0m\n"
 sudo apt-get install -y git
+
+# Vim
+echo -e "\n\033[1;34m  ---------- install vim ----------\033[0;0m\n"
+sudo apt-get install -y vim
 
 # Vim config
 echo -e "\n\033[1;34m  ---------- create vim config ----------\033[0;0m\n"
